1500446aeedd96304bd93c519a2705ac1b12a361
Prabhat-git-99 <prabatwork3939@gmail.com>
2022-04-18 11:45:49 +0530

    first commit

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.mvc.kafka_producer_wikimedia;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ WikimediaChangesProducer.java
@@ -0,0 +1,9 @@
+package com.mvc.kafka_producer_wikimedia;
+
+public class WikimediaChangesProducer {
+	
+	public static void main( String[ ] args ) {
+		System.out.println( "Hey this is project time!" );
+	}
+
+}
