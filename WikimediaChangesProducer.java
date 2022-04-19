dd1e2b03c49a169d2e71567bbbcd2462bfc15c3e
Prabhat-git-99 <prabatwork3939@gmail.com>
2022-04-19 16:53:59 +0530

    Eventhandler added

--- WikimediaChangesProducer.java
+++ WikimediaChangesProducer.java
@@ -1,14 +1,19 @@
 package com.mvc.kafka_producer_wikimedia;
 
+import java.net.URI;
 import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+
 public class WikimediaChangesProducer {
 	
-	public static void main( String[ ] args ) {
+	public static void main( String[ ] args ) throws InterruptedException {
 		System.out.println( "Hey this is project time!" );
 		
 		String bootstrapServers = "127.0.0.1:9092";
@@ -24,6 +29,16 @@ public class WikimediaChangesProducer {
 		
 		String topic = "wikimedia.recentchange";
 		
+		EventHandler eventHandler = new WikimediaChangeHandler( producer, topic );
+		String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+		EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create( url ) );
+		EventSource eventSource = builder.build( );
+		
+		// Start the producer in another thread
+		eventSource.start( );
+		
+		// we can produce for 10mins and block program until then
+		TimeUnit.MINUTES.sleep( 10 );
 		
 	}
 
